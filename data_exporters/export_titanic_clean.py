6242b754af10af634cf58440066110ffbe7cadd8
melih-ekici <root@3e10cdd3a2e8>
2025-05-14 13:42:53 +0000

    Added prints

--- data_exporters/export_titanic_clean.py
+++ data_exporters/export_titanic_clean.py
@@ -1,5 +1,6 @@
 from mage_ai.io.file import FileIO
 from pandas import DataFrame
+import pandas as pd
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
@@ -14,3 +15,7 @@ def export_data_to_file(df: DataFrame, **kwargs) -> None:
     """
     filepath = 'titanic_clean.csv'
     FileIO().export(df, filepath)
+    df = pd.read_csv('titanic_clean.csv')
+    import os
+    print(os.getcwd())
+    print(df.head())
